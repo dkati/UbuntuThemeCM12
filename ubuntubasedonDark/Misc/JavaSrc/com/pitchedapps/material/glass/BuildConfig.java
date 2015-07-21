0ae2fecb8d30be8c4e61c70b6127f8291b73895e
dimitris katikaridis <dkatikaridis@gmail.com>
2015-07-21 14:50:14 +0300

    Successfully edited DarkSide theme to ubuntu

---
+++ ubuntubasedonDark/Misc/JavaSrc/com/pitchedapps/material/glass/BuildConfig.java
@@ -0,0 +1,11 @@
+package com.pitchedapps.material.glass;
+
+public final class BuildConfig
+{
+    public static final String APPLICATION_ID = "com.pitchedapps.material.glass.free";
+    public static final String BUILD_TYPE = "release";
+    public static final boolean DEBUG = false;
+    public static final String FLAVOR = "glass_free";
+    public static final int VERSION_CODE = 1;
+    public static final String VERSION_NAME = "0.0.1";
+}
